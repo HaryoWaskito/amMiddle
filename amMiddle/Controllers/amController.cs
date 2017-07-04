8e9be493f3a204bd14b296d5d3b909a4d44250d9
Haryo Waskito <haryo.way@gmail.com>
2017-07-04 21:36:07 +0700

    Make hidden web api form

--- amMiddle/Controllers/amController.cs
+++ amMiddle/Controllers/amController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using amMiddle.Models;
 using Microsoft.AspNetCore.Mvc;
-using amMiddle.Models;
+using System.Linq;
 
 namespace amMiddle.Controllers
 {
@@ -35,40 +32,6 @@ namespace amMiddle.Controllers
             _context.SaveChanges();
 
             return CreatedAtRoute("Responses", new { id = item.amModelId }, item);
-        }
-
-        #region Commented Sample Code
-        //// GET api/values
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
-
-        //// GET api/values/5
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        //// POST api/values
-        //[HttpPost]
-        //public void Post([FromBody]string value)
-        //{
-        //}
-
-        //// PUT api/values/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody]string value)
-        //{
-        //}
-
-        //// DELETE api/values/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //} 
-        #endregion
+        }        
     }
 }
--- amMiddle/Program.cs
+++ amMiddle/Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 namespace amMiddle
 {
@@ -13,12 +9,12 @@ namespace amMiddle
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
+                    .UseKestrel()
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseIISIntegration()
+                    .UseStartup<Startup>()
+                    .UseApplicationInsights()
+                    .Build();
 
             host.Run();
         }
